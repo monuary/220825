4dbe57b8de79e9b1655b262468e6124030c0128f
Minsang Choi <msc7410@gmail.com>
2022-08-25 01:01:19 +0900

    초기 커밋.

---
+++ Aa.h
@@ -0,0 +1,2 @@
+#pragma once
+void Aa();//헤더 파일에서 함수 선언
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Aa.h"
+
+int main()
+{
+	char AA;
+	Aa();//Aa.h에서 미리 선언해둔 함수
+	std::cout << AA << std::endl;
+	return 0;
+}
+
+//출력이 황당한 숫자로 나온다. 알지 못하는 오류가 있는 모양.
